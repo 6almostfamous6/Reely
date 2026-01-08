d8f1a10658f4802b10040b22d2a09c649781c739
6almostfamous6 <6almostfamous6@gmail.com>
2026-01-08 02:38:20 -0600

    Update Home.tsx

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,1 +1,17 @@
+import '../styles/feed.css';
 
+import React from 'react';
+import '../styles/feed.css';
+import Timeline from '../components/Timeline';
+import CreatePost from '../components/CreatePost';
+
+export default function Home() {
+  return (
+    <div className="home">
+      <div className="feed-container">
+        <CreatePost />
+        <Timeline />
+      </div>
+    </div>
+  );
+}
